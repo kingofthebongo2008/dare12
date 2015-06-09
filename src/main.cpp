1bc85cc87021eb851c53a032253a2784cc561741
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2015-06-09 21:44:44 +0300

    first iimports

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "precompiled.h"
 #include <cstdint>
 
-
+#include "distance.h"
+#include "voisinage.h"
 
 int32_t main( int argc, char const* argv[] )
 {
--- src/voisinage.h
+++ src/voisinage.h
@@ -7,8 +7,8 @@ namespace gpu_deformation
 {
     inline void voisinage(float x, float y, int32_t v1[], int32_t v2[])
     {
-        x = floorf(x);
-        y = floorf(y);
+        int32_t x1 = static_cast<int32_t> (floorf(x));
+        int32_t y1 = static_cast<int32_t> (floorf(y));
 
         //% Returns the "pixelique" coordinates of the point neighborhood( its size is 9 * 9 )? 8x8?
 
@@ -17,12 +17,12 @@ namespace gpu_deformation
 
         for ( uint32_t i = 0; i < 8; ++i )
         {
-            v1[i] = x + indices_v1[i];
+            v1[i] = x1 + indices_v1[i];
         }
 
         for (uint32_t i = 0; i < 8; ++i)
         {
-            v2[i] = y + indices_v2[i];
+            v2[i] = y1 + indices_v2[i];
         }
 
     }
