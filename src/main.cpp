dfee18c5b04aaeedb0339d9da393a1eee1280821
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2015-06-09 21:03:13 +0300

    made first example to work

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,9 @@
 #include "precompiled.h"
 #include <cstdint>
 
-#include "cuda_helper.h"
 
-
-static void initialize_cuda()
-{
-    //initialize cuda
-    cuda::check_device_capabilites(3, 0, []()->void
-    {
-        std::cout << "Cuda memory system initialization...." << std::endl;
-    }
-        , []()->void
-    {
-      });
-}
-
-extern void test_cuda4();
 
 int32_t main( int argc, char const* argv[] )
 {
-    initialize_cuda();
-    test_cuda4();
     return 0;
 }
