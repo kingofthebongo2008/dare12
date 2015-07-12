8ee6b73183f14801321a0965e4ce970eaa2336f3
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2015-07-12 22:08:47 +0300

    fix bug with incorrect ordering of the free form contour

--- src/cuda_split.cu
+++ src/cuda_split.cu
@@ -3,11 +3,14 @@
 #include "freeform_patch.h"
 
 #include <thrust/transform.h>
+#include <thrust/sort.h>
 
 #include <math/math_vector.h>
 
 #include "math_functions.h"
 
+
+#include "cuda_print_utils.h"
 
 #include <algorithm>
 
@@ -18,18 +21,23 @@ namespace freeform
         float                          m_seuil;
         thrust::device_ptr<patch>      m_patches;
         thrust::device_ptr<uint32_t>   m_element_count;
+        thrust::device_ptr<uint32_t>   m_keys;
 
-        multi_eval_patches2_kernel(float seuil, thrust::device_ptr<patch> patches, thrust::device_ptr<uint32_t> element_count) :
+        multi_eval_patches2_kernel(float seuil, thrust::device_ptr<patch> patches, thrust::device_ptr<uint32_t> element_count, thrust::device_ptr<uint32_t> key) :
             m_seuil(seuil)
             , m_patches(patches)
             , m_element_count(element_count)
+            , m_keys(key)
         {
 
         }
 
         //split procedure
-        __device__ void operator() (const patch & p) const
+        __device__ void operator() (const thrust::tuple<patch, uint32_t>& t) const
         {
+            auto p = thrust::get<0>(t);
+            auto key = thrust::get<1>(t);
+
             auto d_0 = math::distance(p.x0, p.y0, p.x1, p.y1);
             auto d_1 = math::distance(p.x1, p.y1, p.x2, p.y2);
             auto d_2 = math::distance(p.x2, p.y2, p.x3, p.y3);
@@ -51,42 +59,59 @@ namespace freeform
 
                 m_patches[old] = g4;
                 m_patches[old + 1] = g5;
+
+                //store the index number, so we can sort later to maintain the order
+                m_keys[old] = key;
+                m_keys[old + 1] = key;
             }
             else
             {
                 auto old = atomicAdd(m_element_count.get(), 1);
                 m_patches[old] = p;
+                m_keys[old] = key;
             }
         }
     };
 
-    //thrust::copy(g.begin(), g.end(), std::ostream_iterator< float >(std::cout, " "));
-
     //split procedure, generates new patches if they are too close
     patches split(const patches& p)
     {
-        patches                      n2;
-        thrust::device_vector<uint32_t> element_count;
-        thrust::device_vector<uint32_t> host_element_count;
+        using namespace thrust;
+
+        patches                 n2;
+        device_vector<uint32_t> element_count;
+        device_vector<uint32_t> host_element_count;
+        device_vector<uint32_t> keys;
 
         auto maxi = p.size();
+        auto s = p.size();
 
-        n2.resize( p.size() * 2 );
+        n2.resize( s * 2 );
+        keys.resize(s * 2);
 
         element_count.resize(1);
         host_element_count.resize(1);
 
-        auto b = p.begin();
-        auto e = p.end();
+        auto cb = make_counting_iterator(0);
+        auto ce = cb + s;
 
-        thrust::for_each(b, e, multi_eval_patches2_kernel(45, &n2[0], &element_count[0]));
+        auto b = make_zip_iterator(make_tuple(p.begin(), cb));
+        auto e = make_zip_iterator(make_tuple(p.end(), ce));
 
+        for_each(b, e, multi_eval_patches2_kernel(32, &n2[0], &element_count[0], &keys[0]));
+
+        //fetch the number of new patches that were added
         auto elements = element_count.front();
-
-        thrust::copy_n(element_count.begin(), 1, host_element_count.begin());
-
+        copy_n(element_count.begin(), 1, host_element_count.begin());
         uint32_t new_size = host_element_count[0];
+        
+        //resize the arrays
         n2.resize(new_size);
+        keys.resize(new_size);
+
+        //the patches in the free form countour have order which must be maintained
+        sort_by_key(keys.begin(), keys.end(), n2.begin());
+
         return n2;
     }
 }
